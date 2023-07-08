12e9f03cf47a5310497200cbf9db1300658b8483
Zehan Yang <zehan.yang@uconn.edu>
2023-07-07 22:53:50 -0400

    udpate time

--- R/functions.R
+++ R/functions.R
@@ -51,12 +51,14 @@ semi_rk_ssp <- function(x, y, delta, r0, ssp_type, alpha) {
   n <- nrow(x)
   if (ssp_type == "uniform") {
     ssp <- rep(1 / n, n)
-    return(list(ssp = ssp, index.pilot = sample(n, r0, TRUE), 
-                converge = 0))
+    return(list(
+      ssp = ssp, index.pilot = sample(n, r0, TRUE),
+      converge = 0
+    ))
   } else {
     ssp_pt <- rep(1 / n, r0)
     ind_pt <- sample(n, r0, TRUE) # index of pilot sample
-    x_pt <- x[ind_pt, ] 
+    x_pt <- x[ind_pt, ]
     y_pt <- y[ind_pt]
     dt_pt <- delta[ind_pt]
     mle_pt <- semi_rk_est(x_pt, y_pt, dt_pt, ssp_pt, n) # pilot estimator
@@ -76,8 +78,10 @@ semi_rk_ssp <- function(x, y, delta, r0, ssp_type, alpha) {
       m_mse <- sqrt(colSums((tcrossprod(m_inv, g))^2))
       ssp <- m_mse / sum(m_mse) * (1 - alpha) + alpha / n # optA SSPs
     }
-    return(list(ssp = ssp, est.pilot = bt_pt, 
-                index.pilot = ind_pt, M = m, converge = 0))
+    return(list(
+      ssp = ssp, est.pilot = bt_pt,
+      index.pilot = ind_pt, M = m, converge = 0
+    ))
   }
 }
 
@@ -99,53 +103,59 @@ semi_rk_fit <- function(x, y, delta, r0, r, ssp_type, se = TRUE, alpha = 0.2) {
   }
   pi <- ssps$ssp # optimal SSPs
   ind_r <- sample(n, r, prob = pi, replace = TRUE) # index of second-step subsample
-  if (ssp_type == "uniform") {
-    ind_st <- c(ind_r, ssps$index.pilot)
-    # using uniform SSPs as the control group
-    # combine pilot and second-step subsample to derive the final estimator
-    t_est <- system.time(est <- semi_rk_est(x[ind_st, ], y[ind_st],
-                                            delta[ind_st], rep(1/n, (r+r0)), n))
-    coe_out <- as.vector(est$coefficient)
-  }else {
-    # second-step estimator
-    t_est <- system.time(est <- semi_rk_est(x[ind_r, ], y[ind_r], 
-                                            delta[ind_r], pi[ind_r], n))
-    coe_sec <- as.vector(est$coefficient)
-    # hessian matrix estimated by the second step subsample
-    m_sec <- r * gehan_s_jaco(x[ind_r, ], y[ind_r], delta[ind_r],
-                          pi[ind_r], coe_sec, n)
-    m_pt <- r0 * ssps$M
-    # aggregate the pilot and second-step estimator
-    coe_out <- drop(solve(m_pt + m_sec) %*% (m_pt %*% ssps$est.pilot + m_sec %*% coe_sec))
-  }
+  t_est <- system.time({
+    if (ssp_type == "uniform") {
+      ind_st <- c(ind_r, ssps$index.pilot)
+      # using uniform SSPs as the control group
+      # combine pilot and second-step subsample to derive the final estimator
+      est <- semi_rk_est(x[ind_st, ], y[ind_st], delta[ind_st], rep(1 / n, (r + r0)), n)
+      coe_out <- as.vector(est$coefficient)
+    } else {
+      # second-step estimator
+      est <- semi_rk_est(x[ind_r, ], y[ind_r], delta[ind_r], pi[ind_r], n)
+      coe_sec <- as.vector(est$coefficient)
+      # hessian matrix estimated by the second step subsample
+      m_sec <- r * gehan_s_jaco(
+        x[ind_r, ], y[ind_r], delta[ind_r],
+        pi[ind_r], coe_sec, n
+      )
+      m_pt <- r0 * ssps$M
+      # aggregate the pilot and second-step estimator
+      coe_out <- drop(solve(m_pt + m_sec) %*% (m_pt %*% ssps$est.pilot + m_sec %*% coe_sec))
+    }
+  })
   iter <- est$iter
   if (est$converge %in% c(1, 2)) {
     stop(paste0("Fail to get a converging second-step
     estimator. The converging code is ", est$converge))
   }
-  t_se <- system.time(
-  {if (se) {
-    ind_st <- c(ind_r, ssps$index.pilot) # combined subsample index
-    pi_st <- c(pi[ind_r], rep(1/n, r0)) # combined SSPs
-    # estimating function estimated by the combined subsample
-    g <- gehan_s_mtg(
-      x[ind_st, ], y[ind_st], delta[ind_st], pi_st,
-      coe_out, seq_along(ind_st) - 1, n
-    )
-    # estimate vc
-    vc <- crossprod(g) * r
-    vc_add <- crossprod(pi[ind_st] * g, g) * r * n
-    vc_amend <- vc + (r / n) * vc_add
-    # inverse of the hessian matrix estimated by the second step subsample
-    m_inv <- solve(gehan_s_jaco(
-      x[ind_st, ], y[ind_st], delta[ind_st], pi_st, coe_out, n))
-    vx <- m_inv %*% vc %*% m_inv / r # sandwich estimator for full estimator
-    std <- c(sqrt(diag(vx)))
-    vx_amend <- m_inv %*% vc_amend %*% m_inv / r # sandwich estimator for true coe
-    std_amend <- c(sqrt(diag(vx_amend)))
-  } else {
-    std <- std_amend <- NA
-  }})
+  t_se <- system.time({
+    if (se) {
+      ind_st <- c(ind_r, ssps$index.pilot) # combined subsample index
+      pi_st <- c(pi[ind_r], rep(1 / n, r0)) # combined SSPs
+      # estimating function estimated by the combined subsample
+      g <- gehan_s_mtg(
+        x[ind_st, ], y[ind_st], delta[ind_st], pi_st,
+        coe_out, seq_along(ind_st) - 1, n
+      )
+      # estimate vc
+      vc <- crossprod(g) * r
+      vc_add <- crossprod(pi[ind_st] * g, g) * r * n
+      vc_amend <- vc + (r / n) * vc_add
+      # inverse of the hessian matrix estimated by the second step subsample
+      m_inv <- solve(gehan_s_jaco(
+        x[ind_st, ], y[ind_st], delta[ind_st], pi_st, coe_out, n
+      ))
+      # sandwich estimator for full estimator
+      vx <- m_inv %*% vc %*% m_inv / r 
+      std <- c(sqrt(diag(vx)))
+      # sandwich estimator for true coe
+      vx_amend <- m_inv %*% vc_amend %*% m_inv / r 
+      std_amend <- c(sqrt(diag(vx_amend)))
+    } else {
+      std <- std_amend <- NA
+    }
+  })
   if (is.null(colnames(x))) {
     names(coe_out) <- paste0("Beta", seq_len(ncol(x)))
     names(std) <- names(std_amend) <- paste0("Beta", seq_len(ncol(x)))
@@ -154,14 +164,20 @@ semi_rk_fit <- function(x, y, delta, r0, r, ssp_type, se = TRUE, alpha = 0.2) {
     names(std) <- names(std_amend) <- colnames(x)
   }
   # add an intercept term if elements in the first column in x are 1's
-  if (intercept) {
-    tmp <- names(coe_out)
-    coe_out <- c(max(eres((y[ind_st] - x[ind_st, ] %*% coe_out), 
-                          delta[ind_st], pi_st, (r+r0))[[2]]), coe_out)
-    names(coe_out) <- c("intercept", tmp)
-  }
-  time <- cbind(t_ssp, t_est, t_se)
+  t_est_itcpt <- system.time({
+    if (intercept) {
+      tmp <- names(coe_out)
+      coe_out <- c(max(eres(
+        (y[ind_st] - x[ind_st, ] %*% coe_out),
+        delta[ind_st], pi_st, (r + r0)
+      )[[2]]), coe_out)
+      names(coe_out) <- c("intercept", tmp)
+    }
+  })
+  time <- cbind(t_ssp, t_est + t_est_itcpt, t_se)
   colnames(time) <- c("SSPs", "Est", "SE")
-  return(list(coe = coe_out, std = std, std_amend = std_amend, iter = iter, 
-              converge = 0, time = time))
+  return(list(
+    coe = coe_out, std = std, std_amend = std_amend,
+    iter = iter, converge = 0, time = time
+  ))
 }
